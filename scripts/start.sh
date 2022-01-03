e53d894aee5b2595717af04826a6c4b379f66678
59 <5.9.devv@gmail.com>
2022-01-04 00:28:57 +0900

    codeDeploy 12차수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=thandbag
+PROJECT_NAME=thandbag-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
