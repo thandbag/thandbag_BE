12b25d74a47189a45e80f153323a8f82e916c7f3
59 <5.9.devv@gmail.com>
2022-01-03 21:30:00 +0900

    codeDeploy 7차수정

--- src/test/java/com/example/thandbag/ThandbagApplicationTests.java
+++ src/test/java/com/example/thandbag/ThandbagApplicationTests.java
@@ -3,11 +3,6 @@ package com.example.thandbag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ThandbagApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
