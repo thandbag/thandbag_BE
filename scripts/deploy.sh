1051989ef7171b220bc5e66c8b41346ced06e1cc
59 <5.9.devv@gmail.com>
2022-01-04 02:05:23 +0900

    codeDeploy 18차수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
