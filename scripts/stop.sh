7cf1383dc7d9af4516f2624aab72e2a52b9fdba0
59 <5.9.devv@gmail.com>
2022-01-06 21:59:47 +0900

    sh파일 일부 수정 테스트

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,9 +8,11 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+echo "> 현재 IDLE_PID : ${IDLE_PID}"
 
 if [ -z ${IDLE_PID} ]
 then
+  echo "> 구동중인 PID : ${IDLE_PID}"
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
