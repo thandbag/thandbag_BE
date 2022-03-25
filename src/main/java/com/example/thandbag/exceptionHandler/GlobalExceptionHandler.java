d090747d5585a6c13f3f7662a1f3745a5b5a803d
davidko <ksb3602@gmail.com>
2022-03-26 00:20:18 +0900

    400 에러 처리 부분 변경

--- src/main/java/com/example/thandbag/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/thandbag/exceptionHandler/GlobalExceptionHandler.java
@@ -3,21 +3,16 @@ package com.example.thandbag.exceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = {IllegalArgumentException.class})
-    public ResponseEntity<Object> handleApiRequestException(IllegalArgumentException ex) {
-        RestApiException restApiException = new RestApiException();
-        restApiException.setHttpStatus(HttpStatus.BAD_REQUEST);
-        restApiException.setErrorMessage(ex.getMessage());
-
-        return new ResponseEntity(
-                restApiException,
-                HttpStatus.BAD_REQUEST
-        );
+    public RestApiException handleApiRequestException(IllegalArgumentException ex) {
+        return new RestApiException(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 }
--- src/main/java/com/example/thandbag/exceptionHandler/RestApiException.java
+++ src/main/java/com/example/thandbag/exceptionHandler/RestApiException.java
@@ -1,13 +1,15 @@
 package com.example.thandbag.exceptionHandler;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class RestApiException {
 
-    private String errorMessage;
     private HttpStatus httpStatus;
+    private String errorMessage;
+
 }
